cd3910aaa1b08e5c5943cc70a62964ba83f5c756
Prav <larchpraveen@gmail.com>
2018-11-20 22:39:11 +0000

    yeh

--- index.py
+++ index.py
@@ -12,7 +12,6 @@ from threading import Thread
 from column_transposition import *
 app = Flask(__name__)
 
-text = ""
 
 @app.route('/')
 def my_form():
@@ -20,13 +19,12 @@ def my_form():
 
 @app.route('/', methods=['GET', 'POST'])
 def my_form_post():
-    global text
     text = request.form['text']
-    return redirect(url_for('my_methods'))
+    return redirect(url_for('my_methods', yourtext=text))
 
-@app.route('/substitution')
+@app.route('/substitution', methods=['GET', 'POST'])
 def substitution():
-    global text
+    text = request.args.get("yourtext")
     userinput = text.upper()
     key = string.ascii_uppercase
     ciphertype = "substitution"
@@ -38,7 +36,7 @@ def substitution():
 
 @app.route('/columntrans', methods=['GET', 'POST'])
 def columntrans():
-    global text
+    text = request.args.get("yourtext")
     userinput = text.upper()
     regex = re.compile('[^A-Z]')
     colno = int(request.get_json())
@@ -57,16 +55,8 @@ def columntrans():
 
 @app.route('/methods')
 def my_methods():
-    return render_template('methods.html')
-
-@app.route('/methods', methods=['GET', 'POST'])
-def my_methods_post():
-    global text
-    userinput = text.upper()
-    key = string.ascii_uppercase
-    ciphertype = "substitution"
-    regex = re.compile('[^A-Z]')
-    return(annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", ""))    
+    yourtext = request.args.get("yourtext")
+    return render_template('methods.html', yourtext=yourtext)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
